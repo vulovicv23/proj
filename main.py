d52e02af0787d915f43be7089be72c78c26e8e3f
Vukasin Vulovic <vulovicv@dire-solutions.com>
2022-03-15 17:16:28 +0100

    Added new print

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 def main():
     print("Hello World!")
+    print("Hello World!")
+    print("Hello World!")
+    print("Hello World!")
+    print("Hello World!")
+    print("Hello World!")
+    print("Hello World!")
 
 if __name__ == "__main__":
     main()
